0c1bac9107de3c6f68033be0be4cc05328fba660
Shudhanshu7 <149239101+Shudhanshu7@users.noreply.github.com>
2024-10-24 00:49:14 +0530

    Update TripList.jsx

--- src/pages/TripList.jsx
+++ src/pages/TripList.jsx
@@ -17,7 +17,7 @@ const TripList = () => {
   const getTripList = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3001/users/${userId}/trips`,
+        `https://rental-app-99fz.vercel.app/users/${userId}/trips`,
         {
           method: "GET",
         }
