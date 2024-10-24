2ed9f109b41aac990aa28b953e3a39e9504e73e6
Shudhanshu7 <149239101+Shudhanshu7@users.noreply.github.com>
2024-10-25 00:57:21 +0530

    Update Listings.jsx

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -18,8 +18,8 @@ const Listings = () => {
     try {
       const response = await fetch(
         selectedCategory !== "All"
-          ? `https://rental-app-orpin.vercel.app/properties?category=${selectedCategory}`
-          : "https://rental-app-orpin.vercel.app/properties",
+          ? `https://rental-app-2.onrender.com/properties?category=${selectedCategory}`
+          : "https://rental-app-2.onrender.com/properties",
         {
           method: "GET",
         }
