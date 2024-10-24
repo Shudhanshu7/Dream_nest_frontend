0ed4c1fe64ebdb508561373d6700fcd1af360d63
Shudhanshu7 <149239101+Shudhanshu7@users.noreply.github.com>
2024-10-25 01:01:27 +0530

    Update TripList.jsx

--- src/pages/TripList.jsx
+++ src/pages/TripList.jsx
@@ -17,7 +17,7 @@ const TripList = () => {
   const getTripList = async () => {
     try {
       const response = await fetch(
-        `https://rental-app-orpin.vercel.app/users/${userId}/trips`,
+        `https://rental-app-2.onrender.com/users/${userId}/trips`,
         {
           method: "GET",
         }
